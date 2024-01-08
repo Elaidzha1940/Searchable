329a687085377c004bb9dd36d9406c4428183d6e
Elaidzha1940 <eli080293@gmail.com>
2024-01-09 00:09:12 +0300

    Update SearchableViewModel

--- Searchable/ContentView.swift
+++ Searchable/ContentView.swift
@@ -9,11 +9,64 @@
 
 import SwiftUI
 
+struct Restaurant: Identifiable, Hashable {
+    let id: String
+    let title: String
+    let cuisine: CuisineOption
+}
+
+enum CuisineOption: String {
+    case russian, georgian, italian, french, japanese
+}
+
+final class RestaurantManager {
+    
+    func getAllRestaurants() async throws -> [Restaurant] {
+        [
+            Restaurant(id: "1", title: "Khinkali", cuisine: .georgian),
+            Restaurant(id: "2", title: "Syniki", cuisine: .russian),
+            Restaurant(id: "3", title: "Lasania", cuisine: .italian),
+            Restaurant(id: "4", title: "Ratatouille", cuisine: .french),
+            Restaurant(id: "5", title: "Ramen", cuisine: .japanese),
+        ]
+    }
+}
+
+@MainActor
+final class SearchableViewModel: ObservableObject {
+    @Published private(set) var allRestaurants: [Restaurant] = []
+    let manager = RestaurantManager()
+    
+    func loadRestaurants() async {
+        do {
+            allRestaurants = try await manager.getAllRestaurants()
+        } catch {
+            print(error)
+        }
+    }
+}
+
 struct Searchable: View {
+    @StateObject private var viewModel = SearchableViewModel()
+    
     var body: some View {
         
-        VStack {
-           
+        ScrollView {
+            VStack(spacing: 20) {
+                ForEach(viewModel.allRestaurants) { restaurant in
+                    
+                }
+            }
+        }
+        .task {
+            await viewModel.loadRestaurants()
+        }
+    }
+    
+    private func restaurantRow(restaurant: Restaurant) -> some View {
+        VStack(alignment: .leading, spacing: 10) {
+            Text(restaurant.title)
+            Text(restaurant.cuisine.rawValue.capitalized)
         }
     }
 }
