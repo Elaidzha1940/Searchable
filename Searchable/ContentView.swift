add2dbeea68c3f33727194f5cd8bd833dcc29cc5
Elaidzha1940 <eli080293@gmail.com>
2024-01-08 23:40:05 +0300

    Searchable

---
+++ Searchable/ContentView.swift
@@ -0,0 +1,23 @@
+//  /*
+//
+//  Project: Searchable
+//  File: Searchable.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 08.01.2024
+//
+//  */
+
+import SwiftUI
+
+struct Searchable: View {
+    var body: some View {
+        
+        VStack {
+           
+        }
+    }
+}
+
+#Preview {
+    Searchable()
+}
---
+++ Searchable/SearchableApp.swift
@@ -0,0 +1,19 @@
+//  /*
+//
+//  Project: Searchable
+//  File: SearchableApp.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 08.01.2024
+//
+//  */
+
+import SwiftUI
+
+@main
+struct SearchableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            Searchable()
+        }
+    }
+}
